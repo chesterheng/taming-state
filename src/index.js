3b321317271939eddc7a29f4814ac4b0336b5329
Chester Heng <chesterheng@ip-192-168-6-21.ap-southeast-1.compute.internal>
2019-07-16 15:01:44 +0800

    09:basic redux store, actions and reducers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
@@ -10,3 +11,54 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "TODO_ADD": {
+      return applyAddTodo(state, action);
+    }
+    case "TODO_TOGGLE": {
+      return applyToggleTodo(state, action);
+    }
+    default:
+      return state;
+  }
+};
+
+const applyAddTodo = (state, action) => {
+  return state.concat(action.todo);
+};
+
+const applyToggleTodo = (state, action) => {
+  return state.map(todo =>
+    todo.id === action.todo.id
+      ? Object.assign({}, todo, { completed: !todo.completed })
+      : todo
+  );
+};
+
+const store = createStore(reducer, []);
+console.log("initial state:");
+console.log(store.getState());
+
+const unsubscribe = store.subscribe(() => {
+  console.log("store update, current state:");
+  console.log(store.getState());
+});
+
+store.dispatch({
+  type: "TODO_ADD",
+  todo: { id: "0", name: "learn redux", completed: false }
+});
+
+store.dispatch({
+  type: "TODO_ADD",
+  todo: { id: "1", name: "learn mobx", completed: false }
+});
+
+store.dispatch({
+  type: "TODO_TOGGLE",
+  todo: { id: "0" }
+});
+
+unsubscribe();
