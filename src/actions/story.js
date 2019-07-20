30864bada04209258d0670078c45c321c382d7cd
Chester Heng <chesterheng@Chesters-MacBook-Pro.local>
2019-07-20 17:06:07 +0800

    15-16:Error Handling

--- src/actions/story.js
+++ src/actions/story.js
@@ -1,7 +1,16 @@
-import { STORIES_ADD, STORIES_FETCH } from "../constants/actionTypes";
+import {
+  STORIES_ADD,
+  STORIES_FETCH,
+  STORIES_FETCH_ERROR
+} from "../constants/actionTypes";
 
 const doAddStories = stories => ({ type: STORIES_ADD, stories });
 
 const doFetchStories = query => ({ type: STORIES_FETCH, query });
 
-export { doAddStories, doFetchStories };
+const doFetchErrorStories = error => ({
+  type: STORIES_FETCH_ERROR,
+  error
+});
+
+export { doAddStories, doFetchStories, doFetchErrorStories };
