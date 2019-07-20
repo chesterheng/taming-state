c6fe2b69f36a4c294bd25783936380387a614bc5
Chester Heng <chesterheng@Chesters-MacBook-Pro.local>
2019-07-20 16:15:51 +0800

    15-12:Connect React with Redux

--- src/components/Stories.js
+++ src/components/Stories.js
@@ -1,4 +1,7 @@
 import React from "react";
+import { connect } from "react-redux";
+import { doArchiveStory } from "../actions/archive";
+import { getReadableStories } from "../selectors/story";
 import "./Stories.css";
 import Story from "./Story";
 import StoriesHeader from "./StoriesHeader";
@@ -25,7 +28,7 @@ const COLUMNS = {
   }
 };
 
-const Stories = ({ stories, onArchive }) => (
+const Stories = ({ stories, doArchiveStory }) => (
   <div className="stories">
     <StoriesHeader columns={COLUMNS} />
     {(stories || []).map(story => (
@@ -33,9 +36,17 @@ const Stories = ({ stories, onArchive }) => (
         key={story.objectID}
         story={story}
         columns={COLUMNS}
-        onArchive={onArchive}
+        onArchive={doArchiveStory}
       />
     ))}
   </div>
 );
-export default Stories;
+
+const mapStateToProps = state => ({
+  stories: getReadableStories(state)
+});
+
+export default connect(
+  mapStateToProps,
+  { doArchiveStory }
+)(Stories);
