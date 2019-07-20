6aa0618b7645d300ed63e2857914f05d5b7ef379
Chester Heng <chesterheng@Chesters-MacBook-Pro.local>
2019-07-20 10:03:44 +0800

    15-04:Archive a Story

---
+++ src/components/ButtonInline.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Button from "./Button";
+
+const ButtonInline = ({ onClick, type = "button", children }) => (
+  <Button type={type} className="button-inline" onClick={onClick}>
+    {children}
+  </Button>
+);
+export default ButtonInline;
